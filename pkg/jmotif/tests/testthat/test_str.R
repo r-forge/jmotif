2e82f3dc6afa04bee85ff929fdd9ae6e82a98d3a
seninp (Pavel Senin) <seninp@gmail.com>
2015-10-05 06:44:04 +0000

    adding the code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/jmotif@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/jmotif/tests/testthat/test_str.R
@@ -0,0 +1,16 @@
+test_that("idx to letter", {
+  expect_true(is.character(idx2letter(3)))
+  expect_equal(idx2letter(3), "c")
+})
+
+test_that("letter to idx", {
+  expect_true(is.numeric(letter2idx('c')))
+  expect_equal(letter2idx('c'), 3)
+})
+
+test_that("letters to idx", {
+  expect_true(is.numeric(letters2idx(c('a','b','c','a'))))
+  expect_true(is.vector(letters2idx(c('a','b','c','a'))))
+  expect_equal(length(letters2idx(c('a','b','c','a'))), 4)
+  expect_equal(letters2idx(c('a','b','c','a'))[3], 3)
+})
